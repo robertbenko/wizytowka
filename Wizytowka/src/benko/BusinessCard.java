53a9d805f5ef547d7d40e54e9bd45a7eecba879f
Robert Benko <robert.benko@gmail.com>
2018-10-08 15:46:32 +0200

    1c business card

---
+++ Wizytowka/src/benko/BusinessCard.java
@@ -0,0 +1,39 @@
+package benko;
+
+public class BusinessCard {
+	
+	public static void main(String[] args) {
+	
+	String name = "Robert";
+	String surname = "Ben";
+	int number = 1234;
+	String city = "Krw";
+	String upperLine = name + " " + surname;
+	String underline = number + " " + city;
+	String star = "*";
+	int i;
+	int licznik = 0;
+	int stars;
+
+
+	int underlineLength = underline.length();
+	int upperLineLength = upperLine.length();
+	String empty = "";
+	
+	
+	while(licznik<=underlineLength){
+		 //System.out.print(star);
+		  int g = licznik++;
+		  System.out.print(g);
+	  }
+	
+	System.out.println();
+	System.out.println(star + name + " " + surname);
+	System.out.println(star + underline);
+	
+	for(i=0; i<=underlineLength; i++) {
+		System.out.print(star);
+	}
+	
+	}
+}
---
+++ Wizytowka/src/benko/Wizytowka.java
@@ -0,0 +1,9 @@
+package benko;
+
+
+public class Wizytowka {
+
+	
+	
+	
+}
