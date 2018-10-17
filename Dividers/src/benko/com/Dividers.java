756984f12f8f923331c039b47460109a6b6f73c9
Robert Ben <robert.benko@gmail.com>
2018-10-17 14:54:32 +0200

    1. Dividers

---
+++ Dividers/src/benko/com/Dividers.java
@@ -0,0 +1,70 @@
+package benko.com;
+
+import java.util.Scanner;
+
+public class Dividers {
+
+	public static void main(String[] args) {
+		double a=0;
+		readNumber(a);
+		int [] d = new int[(int) (readNumber(a)/2)];
+		//checkDivider(b);
+		System.out.println(findDividers(d));
+		
+
+		
+	}
+//wczytuje liczbę naturalną ze standardowego wejścia i zwraca ją
+	
+	public static double readNumber(double a) {
+		
+		Scanner skaner = new Scanner(System.in);
+		
+		System.out.println("podaj liczbe do sprawdzenia");
+		a = skaner.nextInt();
+		
+		return a;
+	}
+	
+//sprawdza, czy liczba a dzieli się przez liczbę b.
+	
+//	public static boolean checkDivider(boolean b) {
+//				
+//		double a = 0;
+//		
+//		double x = readNumber(a) % 2;
+//		if( x == 0) {
+//			b = true;
+//		}
+//		else {
+//			b=false;
+//		}
+//										System.out.println(b);
+//		
+//		return b;
+//	}
+	
+//dla zadanej liczby a przechodzi po wszystkich liczbach od 0 do x/2. Jeśli dana liczba jest dzielnikiem a, zapisuje ją w tablicy rezultatów. 
+	
+	public static int[] findDividers(int[] divider) {
+		int dividers=0;
+		double a = 0;
+		int[] d = new int[(int) (readNumber(a)/2)];
+		
+		for(int i=0; i<readNumber(a)/2; i++) {
+			if(readNumber(a) % 2 == 0) {
+				d[i]= dividers + divider[i];
+				
+			}
+		}
+		System.out.println(d);
+		
+		return divider;
+	}
+	
+// wypisuje wszystkie znalezione dzielniki.
+	public static void printDividers() {
+		
+	}
+
+}
