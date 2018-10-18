9e8737b41213024fbe3e6c37fb47e3b71b956d2c
Robert Ben <robert.benko@gmail.com>
2018-10-18 16:05:06 +0200

    3

--- Dividers/src/benko/com/Dividers.java
+++ Dividers/src/benko/com/Dividers.java
@@ -5,69 +5,52 @@ import java.util.Scanner;
 public class Dividers {
 
 	public static void main(String[] args) {
-		double a=0;
-		readNumber(a);
-		int [] d = new int[(int) (readNumber(a)/2)];
+		//double a=0;
+		//readNumber(a);
+		//int[] d = null;
+		//int [] d = new int[(int) (readNumber(a)/2)];
 		//checkDivider(b);
-		System.out.println(findDividers(d));
-		
-
-		
+		//findDividers(d);
+		printDividers();
 	}
 //wczytuje liczbę naturalną ze standardowego wejścia i zwraca ją
 	
 	public static double readNumber(double a) {
 		
-		//Scanner skaner = new Scanner(System.in);
+		Scanner skaner = new Scanner(System.in);
 		
 		//System.out.println("podaj liczbe do sprawdzenia");
-		//a = skaner.nextInt();
-		a = 10;
+		a = skaner.nextInt();
+		//a = 20;
 		return a;
 	}
 	
 //sprawdza, czy liczba a dzieli się przez liczbę b.
 	
-//	public static boolean checkDivider(boolean b) {
-//				
-//		double a = 0;
-//		
-//		double x = readNumber(a) % 2;
-//		if( x == 0) {
-//			b = true;
-//		}
-//		else {
-//			b=false;
-//		}
-//										System.out.println(b);
-//		
-//		return b;
+//public static boolean checkDivider(boolean b) {
+//	
+//	return b;
 //	}
 	
 //dla zadanej liczby a przechodzi po wszystkich liczbach od 0 do x/2. Jeśli dana liczba jest dzielnikiem a, zapisuje ją w tablicy rezultatów. 
 	
 	public static int[] findDividers(int[] divider) {
-		int dividers=0;
-		double a = 10;
-		//int i=0;
-		int[] d = new int[(int) (readNumber(a)/2)];
 		
-		for(int i=0; i<readNumber(a)/2; i++) {
-			if(readNumber(a) % 2 == 0) {
+		double a = 0;
+		for(int i=1; i<=readNumber(a)/2; i++) {
+			if(readNumber(a) % i == 0) {
 				int k= i;
-				d[i]= i+i;
 				System.out.println(k);
 			}
-				
 		}
-		
-		
 		return divider;
 	}
 	
 // wypisuje wszystkie znalezione dzielniki.
 	public static void printDividers() {
-		
+		int[] divider;
+		findDividers(divider);
+		System.out.println(k);
 	}
 
 }
