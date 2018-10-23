c64b28e4f512dcbcca7639b79d501b93d78009ed
Robert Ben <robert.benko@gmail.com>
2018-10-23 16:57:32 +0200

    1 commit Logins

---
+++ Logins/src/benko/com/Logins.java
@@ -0,0 +1,43 @@
+package benko.com;
+
+public class Logins {
+
+	public static void main(String[] args) {
+				
+		String test = "dopodzi:elenia";
+		//String p = test.substring(2,5);
+		//System.out.println(p);
+		
+		//String[] test1 = {"kwasek"};
+		
+//		String[] names = {"piotr budynek", 
+//				"jan nowak",
+//				"karolina Mirek",
+//				"zenon Piwko",
+//				"Maria Kamena"};
+		
+		//for(int i=0; i<5; i++)
+			//names[i] = names.toString();
+	
+		//for(String k:names);
+		//	System.out.println(k);
+		
+		//String[] n = new String[];
+	
+		//String n = names.substring(5,2);
+		//System.out.println(n);
+	String[] aa = test.split(":");
+			
+	System.out.println(aa);
+		
+		
+		
+		
+			
+			
+	}	
+}
+
+
+
+
