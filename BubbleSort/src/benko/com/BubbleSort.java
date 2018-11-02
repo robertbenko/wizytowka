39c162155ff6a4ad7ab4166e6596ebc5cb4e01bb
Robert Ben <robert.benko@gmail.com>
2018-11-02 15:47:59 +0100

    BubbleSort

---
+++ BubbleSort/src/benko/com/BubbleSort.java
@@ -0,0 +1,56 @@
+package benko.com;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+    	printing();
+    }
+    
+    static int[] sortNumbers(int[] b) {
+
+        int[] numbers = new int[] { 4, 2, 0, 2, 10, 1, 9, 12, 3, 5 };
+        int l = numbers.length;
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers.length - 1; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    int temp;
+                    temp = numbers[j + 1];
+                    //System.out.println(temp);
+               
+                    numbers[j + 1] = numbers[j];
+                    //System.out.println(numbers[j+1]);
+                    numbers[j] = temp;
+                    //System.out.println(numbers[j]);
+                }
+            }
+        }
+        return b;
+    }
+        
+    static int jakasLiczba(int a) {
+    	a = 3;
+    	return a;
+    }
+    
+     static void printing() {
+    	 
+    	 int a = jakasLiczba(0);
+         zwieksz(a);
+         System.out.println(a);
+         
+         for (int i = 0; i < zwieksz(a); i++) {
+             System.out.println(zwieksz(a));
+          }
+     }
+     
+     static int zwieksz(int liczba){
+             liczba++;
+             return liczba;
+   
+     }
+ 
+
+}
+
+
